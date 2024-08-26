178430c18a034414c669ec3ff3aa6f17e5f4947d
Akshay Sunil <akshayofficial43@gmail.com>
2024-08-27 02:20:14 +0530

    added the hosted domain name

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 "use client"
 export default function myImageLoader({ src , width , quality }) {
     if(src.startsWith('https://images.pexels.com')) return src
-    return `https://akshaysunilportfolio.netlify.app/${src}?w=${width}&q=${quality || 75}`
+    return `https://akshaysunil-portfolio.vercel.app/${src}?w=${width}&q=${quality || 75}`
 }
