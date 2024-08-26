6527a111f149f7704723b2c1efb035d547987f68
Akshay Sunil <akshayofficial43@gmail.com>
2024-08-27 02:12:35 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,10 +1,9 @@
-{/*"use client"
-export default function myImageLoader({ src , width , quality }) {
+"use client"
+{/*export default function myImageLoader({ src , width , quality }) {
     if(src.startsWith('https://images.pexels.com')) return src
     return `https://akshaynextjsportfolio.netlify.app/${src}?w=${width}&q=${quality || 75}`
 }*/}
 
-"use client"
 export default function myImageLoader({ src , width , quality }) {
     if(src.startsWith('https://images.pexels.com')) return src
     return `https://akshaysunilportfolio.netlify.app/${src}?w=${width}&q=${quality || 75}`
