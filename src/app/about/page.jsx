b3f9198d963ebf468f4769bd03a23dd57bc4e25d
Akshay Sunil <akshayofficial43@gmail.com>
2025-02-18 01:29:33 +0530

    Added skills

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -141,6 +141,14 @@ const AboutPage = () => {
               </div>
 
               <div className=" rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+                Threejs
+              </div>
+
+              <div className=" rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+                Framer Motion
+              </div>
+
+              <div className=" rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
                 Bootstrap
               </div>
 
